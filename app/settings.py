8c80584c05a1234b1a83a2cdafa63291f48a767a
jakeevans <ejake370@gmail.com>
2023-12-09 12:07:47 -0700

    App is working great

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,16 +80,16 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": env('DATABASE_NAME'),
-#         "USER": env('DATABASE_USER'),
-#         "PASSWORD": env('DATABASE_PASS'),
-#         "HOST":env('DATABASE_HOST'),
-#         "PORT":3306,
-#             }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env('DATABASE_NAME'),
+        "USER": env('DATABASE_USER'),
+        "PASSWORD": env('DATABASE_PASS'),
+        "HOST":env('DATABASE_HOST'),
+        "PORT":3306,
+            }
+}
 
 
 # Password validation
